a781baaf0855806082e91b2aad7d276c70d418f8
Lin <Lin@qq.com>
2021-08-11 23:08:17 +0800

    hot-fix commit1

--- src/main/java/com/lin/gittest/gitdemo/Test01.java
+++ src/main/java/com/lin/gittest/gitdemo/Test01.java
@@ -11,5 +11,6 @@ public class Test01 {
         System.out.println("git的测试用例！！！");
         System.out.println("git的测试用例-2！！！");
         System.out.println("git的测试用例-3！！！");
+        System.out.println("git的测试用例-4  update by hot-fix！！！");
     }
 }
