a1ae0814d730d189459d85112af6685ef87dd229
Lin <Lin@qq.com>
2021-08-11 22:51:22 +0800

    first commit

---
+++ src/main/java/com/lin/gittest/gitdemo/Test01.java
@@ -0,0 +1,13 @@
+package com.lin.gittest.gitdemo;
+
+/**
+ * @author lin
+ * @Belongs IntelliJ IDEA
+ * @project gittest
+ * @date 2021/8/11-22:42
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("git的测试用例！！！");
+    }
+}
